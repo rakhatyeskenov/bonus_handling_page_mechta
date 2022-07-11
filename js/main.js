56850cd1e39e8679754892a8b09499a33c483ad5
Rakhat <eskenovrahat@gmail.com>
2022-07-11 23:14:15 +0600

    Fixed mobile_icon show and hide events in mobile version, fixed some links

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-// disable effects on load
-
-// $(window).on('load', function () {
-//   $("body").removeClass("preload");
-// });
-
 // phones on click
 
 document.querySelector('.contacts__btn').addEventListener('click', function() {
-  document.getElementById('one').classList.toggle("contacts__wrap--active");
-  document.getElementById('two').classList.toggle("contacts__wrap--active");
+  document.getElementById('two').classList.toggle("contacts__wrap__active");
 })
+
+let ignoreClickOnMeElement = document.getElementById('one');
+
+document.addEventListener('click', function(event) {
+    var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+    if (!isClickInsideElement) {
+      document.getElementById('two').classList.remove("contacts__wrap__active");
+    }
+});
+
+
 
