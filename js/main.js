9e3e7f1ce656d41a792720f3e4e61921f99e91f4
Rakhat <eskenovrahat@gmail.com>
2022-07-11 16:17:53 +0600

    added links and deleted jquery

--- js/main.js
+++ js/main.js
@@ -4,11 +4,10 @@ $(window).on('load', function () {
   $("body").removeClass("preload");
 });
 
-
 // phones on click
 
-jQuery(document).ready(function(){
-  jQuery('.contacts__btn').click(function(event){
-     jQuery(this).siblings().toggleClass('contacts__wrap--active');
-  });
-});
+function showPhoneNumber() {
+  if (confirm("Нажмите чтобы позвонить:\n+7 (7172) 58 01 58 \nили наберите:\n+7 (7787) 46 00 01")) {
+    window.location = "tel:+77172580158";
+  }
+}
